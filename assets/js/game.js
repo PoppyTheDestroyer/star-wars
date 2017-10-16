7f8f73674b61b2071cb2b9477d21f8124afab357
Jason Saenz <jasonsaenz@jasons-mbp.attlocal.net>
2017-10-15 19:30:35 -0500

    basic pseudo

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+//Define character attribute variables
+
+//Assign character attributes
+
+//Character selection
+
+//Character moves to battlefield
+
+//Enemy selection
+
+//Enemy moves to battlefield
+
+//
